b69bc5579851ddf27797935232fad8538456d9c4
fitzgerald52 <124209843+fitzgerald52@users.noreply.github.com>
2023-05-30 10:58:39 +1200

    i programed the computer to times the input by 7

--- code/1-pupper-age.js
+++ code/1-pupper-age.js
@@ -6,4 +6,6 @@ function pupperAge() {
   alert('Dog years are human years times seven.')
 
   let humanYears = prompt('How old is your dog in human years?')
+  let result = humanYears * 7
+  alert( 'your dog is ' + result + ' years old in dog years !')
 }
