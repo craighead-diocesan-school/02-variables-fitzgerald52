ffebe0213e03d040f490eb802159078a19345a65
fitzgerald52 <124209843+fitzgerald52@users.noreply.github.com>
2023-06-01 10:18:43 +1200

    i coded the program to get the input age and minus it from 83.5

--- code/4-test-life-remaining.js
+++ code/4-test-life-remaining.js
@@ -3,5 +3,8 @@
 // ####################################
 
 function lifeRemaining() {
-  
+  let age =prompt( 'How old are you ?')
+  let lifeexpectancy = 83.5
+  let timeleft = lifeexpectancy - age
+  alert(' you have got ' + timeleft + ' years left in you')
 }
