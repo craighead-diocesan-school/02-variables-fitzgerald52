253d9ee15cf2b060165030b279da7a45d4521e5f
fitzgerald52 <124209843+fitzgerald52@users.noreply.github.com>
2023-06-01 10:14:38 +1200

    i coded the program to times the answer by 3600

--- code/3-lessons-to-seconds.js
+++ code/3-lessons-to-seconds.js
@@ -4,4 +4,7 @@
 
 function lessonsToSeconds() {
   alert('There are 60 minutes in a lesson, and 60 seconds in a minute.')
+  let hours =prompt('How may lessons do you have left today ?')
+let seconds = hours *3600
+alert('That is ' + seconds + ' seconds' )
 }
