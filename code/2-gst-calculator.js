3bb7e3b4c5fd8f5d719f718d08cdac8e98f1eb07
fitzgerald52 <124209843+fitzgerald52@users.noreply.github.com>
2023-05-31 15:17:58 +1200

    I made the program calculate the gst

--- code/2-gst-calculator.js
+++ code/2-gst-calculator.js
@@ -4,4 +4,7 @@
 
 function gstCalculator() {
   alert('The price with GST the price and 15%.')
+let before = prompt('what is the cost before GST ?')
+ let GST = before * 1.15
+ alert('The cost with GST is $' + GST)
 }
